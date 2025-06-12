21621ff6090702ad6c01acf6829ae6f3ffdb39c7
Silas Abayomi <abayomisilas50@gmail.com>
2025-06-12 11:22:07 +0100

    type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import '../app/globals.css'
+// pages/_app.tsx
+import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import '../app/globals.css';
+import React, { ReactElement, ReactNode } from 'react';
 
-export default function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
+// Support for per-page layout
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   return getLayout(<Component {...pageProps} />);
 }
